42816abaff319eee48beb5882f4088b340ba4943
ebbwdan <ebbwdan@gmail.com>
2013-02-26 13:12:31 +1000

    changed method links into buttons. changed edit link on recipe card to a button

--- app/assets/javascripts/recipebinder.js
+++ app/assets/javascripts/recipebinder.js
@@ -3,7 +3,10 @@ RB = {
         $('<div id="recipeInfo" title="Recipe Info"></div>').
             hide().
             appendTo($('body'));   
-        $('#recipes a').click(RB.getRecipeInfo);
+        $('#recipes a').button().click(RB.getRecipeInfo);
+        $('<div id="editrecipeInfo"></div>').
+            hide().
+            appendTo($('body'));
     },
     getRecipeInfo: function() {
         $.ajax({type: 'GET',
@@ -15,14 +18,7 @@ RB = {
         return(false);
     },   
     showRecipeInfo: function(data) {
-        // center a floater 1/2 as wide and 1/4 as tall as screen
-        var oneFourth = Math.ceil($(window).width() / 4);
-        /*$('#recipeInfo').
-            html(data).
-            css({'left': oneFourth,  'width': 2*oneFourth, 'top': 250}).
-            fadeIn('slow').show();
-        // make the Close link in the hidden element work
-        $('#closeLink').click(RB.hideRecipeInfo);*/
+       var oneFourth = Math.ceil($(window).width() / 4);
        $('#recipeInfo').html(data).dialog({
        		autoOpen: false,
        		show: 'slow',
@@ -31,7 +27,32 @@ RB = {
 			'top': 250,
 			modal: true
 			}).dialog('open');
+	$('#editLink').button().click(RB.putRecipeInfo);
         return(false);  // prevent default link action
+    },
+    putRecipeInfo: function() {
+        $.ajax({type: 'PUT',
+                url: $('#recipeInfo').attr('href'),
+                timeout: 5000,
+                success: RB.editRecipeInfo,
+                error: function() { alert('Error!'); }
+               });
+        return(false);
+    },
+    editRecipeInfo: function(data) {
+        // center a floater 1/2 as wide and 1/4 as tall as screen
+        var oneFourth = Math.ceil($(window).width() / 4);
+        $('#recipeInfo').dialog('close');
+        $('#editRecipeInfo').html(data).dialog({
+       		autoOpen: false,
+       		show: 'slow',
+			'left': oneFourth,  
+			'width': 2*oneFourth,
+			'top': 250,
+			modal: true
+			}).dialog('open');
+        //$('#closeLink').click(RB.hideEditRecipeInfo);
+        return(false); // prevent default link action
     }, 
     hideRecipeInfo: function() {
         $('#recipeInfo').hide(); 
@@ -39,3 +60,38 @@ RB = {
     }
 }
 $(RB.setup);       // when document ready, run setup code
+
+/*RB = {
+    setup: function() {
+        $('<div id="editrecipeInfo"></div>').
+            hide().
+            appendTo($('body'));
+        $('#recipes a').click(RB.putRecipeInfo);
+    },
+    putRecipeInfo: function() {
+        $.ajax({type: 'PUT',
+                url: $(this).attr('href'),
+                timeout: 5000,
+                success: RB.editRecipeInfo,
+                error: function() { alert('Error!'); }
+               });
+        return(false);
+    },
+    editRecipeInfo: function(data) {
+        // center a floater 1/2 as wide and 1/4 as tall as screen
+        var oneFourth = Math.ceil($(window).width() / 4);
+        //$("#backgroundPopup").css({"opacity": "0.7"}).fadeIn("slow");
+        //$('#recipeInfo').fadeOut('slow');
+        $('#editrecipeInfo').
+            html(data).
+            css({'left': oneFourth, 'width': 2*oneFourth, 'top': 250}).
+            fadeIn('slow').show();
+        // make the Close link in the hidden element work
+        $('#closeLink').click(RB.hideEditRecipeInfo);
+        return(false); // prevent default link action
+    },
+    hideEditRecipeInfo: function() {
+        $('#editrecipeInfo').hide();
+        return(false);
+    }
+}*/
