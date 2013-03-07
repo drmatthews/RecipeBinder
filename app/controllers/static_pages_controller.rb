64cc2b44db7c52aa21a0226a17de7cd8526f86dc
ebbwdan <ebbwdan@gmail.com>
2013-03-07 12:32:48 +1000

    tweak to home page view to use bootstrap grid

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,7 @@ class StaticPagesController < ApplicationController
   def home
     if signed_in?
       @recipe  = current_user.recipes.build
+      @recipes = current_user.recipes.paginate(page: params[:page])
       @feed_items = current_user.feed.paginate(page: params[:page])
     end
   end
