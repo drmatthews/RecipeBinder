13ae3d4bd3dc4f85601a9ebff2e49d1cf875d3df
ebbwdan <ebbwdan@gmail.com>
2013-03-07 13:14:39 +1000

    added relationship model as a precursor to having followers and being followed

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,11 @@ class User < ActiveRecord::Base
   has_many :recipes, :dependent => :destroy
   has_many :ingredients, :through=> :recipes, :dependent => :destroy
   has_many :steps, :through=> :recipes, :dependent => :destroy
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_relationships, foreign_key: "followed_id",
+                                   class_name:  "Relationship",
+                                   dependent:   :destroy
+  has_many :followed_users, through: :relationships, source: :followed
   before_save { |user| user.email = email.downcase }
   before_create {create_remember_token(:remember_token)} 
 
@@ -29,6 +34,18 @@ class User < ActiveRecord::Base
   def feed
     # This is preliminary. See "Following users" for the full implementation.
     Recipe.where("user_id = ?", id)
+  end
+
+  def following?(other_user)
+    relationships.find_by_followed_id(other_user.id)
+  end
+
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    relationships.find_by_followed_id(other_user.id).destroy
   end
   
   def send_password_reset
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130303080609) do
+ActiveRecord::Schema.define(:version => 20130307024159) do
 
   create_table "ingredients", :force => true do |t|
     t.string   "content"
@@ -29,6 +29,17 @@ ActiveRecord::Schema.define(:version => 20130303080609) do
     t.integer  "user_id"
     t.string   "image"
   end
+
+  create_table "relationships", :force => true do |t|
+    t.integer  "follower_id"
+    t.integer  "followed_id"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
+
+  add_index "relationships", ["followed_id"], :name => "index_relationships_on_followed_id"
+  add_index "relationships", ["follower_id", "followed_id"], :name => "index_relationships_on_follower_id_and_followed_id", :unique => true
+  add_index "relationships", ["follower_id"], :name => "index_relationships_on_follower_id"
 
   create_table "steps", :force => true do |t|
     t.string   "content"
