f096d682a566f04d61b02a33a0538d55c2d0184a
Daniel <daniel@daniel-laptop.(none)>
2013-06-09 10:50:33 +1000

    added mobile device helper method to adjust recipe index to be grid view for mobile phones

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  helper :all
   include SessionsHelper
+    private
+    def mobile_device?
+      request.user_agent =~ /Mobile|webOS/
+    end
+    helper_method :mobile_device?  
 end
